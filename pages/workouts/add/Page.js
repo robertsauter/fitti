b8b979b7b9bb5fce827d8004e9d848f60b11400b
sobertrauter <robert_sauter@gmx.de>
2025-05-10 10:25:28 +0200

    move exercise to separate component

--- pages/workouts/add/Page.js
+++ pages/workouts/add/Page.js
@@ -2,35 +2,24 @@ import '/models/Workout.js';
 import '/models/Exercise.js';
 import '/models/ExerciseResponse.js';
 import { exercisesService } from '/services/ExercisesService.js';
+import { EditExerciseCard } from '../components/EditExerciseCard.js';
 
 export class WorkoutsAddPage extends HTMLElement {
     #ids = {
         exercisesList: 'exercisesList',
         addExerciseButton: 'addExerciseButton',
-        exerciseWrapper: 'exerciseWrapper',
-        upButton: 'upButton',
-        downButton: 'downButton',
-        deleteButton: 'deleteButton',
     }
 
     #inputNames = {
         name: 'name',
-        exercise: 'exercise',
-        sets: 'sets',
     }
 
     #exercisesAmount = 0
-
-    /** @type {ExerciseResponse[]} */
-    #globalExercises = []
-    /** @type {Exercise[]} */
-    #userExercises = []
 
     constructor() {
         super();
 
         this.addExercise = this.addExercise.bind(this);
-        this.deleteExercise = this.deleteExercise.bind(this);
 
         this.attachShadow({ mode: 'open' }).innerHTML = `
             <style>
@@ -53,63 +42,17 @@ export class WorkoutsAddPage extends HTMLElement {
         this.shadowRoot
             .getElementById(this.#ids.addExerciseButton)
             .addEventListener('click', this.addExercise);
-
-        this.#globalExercises = await exercisesService.getGlobalExercises();
-        this.#userExercises = await exercisesService.getUserExercises();
     }
 
     addExercise() {
-        const exerciseWrapper = document.createElement('li');
-        exerciseWrapper.id = `${this.#ids.exerciseWrapper}${this.#exercisesAmount}`;
-
-        const upButtonId = `${this.#ids.upButton}${this.#exercisesAmount}`;
-        const downButtonId = `${this.#ids.downButton}${this.#exercisesAmount}`;
-        const deleteButtonId = `${this.#ids.deleteButton}${this.#exercisesAmount}`;
-        const selectName = `${this.#inputNames.exercise}${this.#exercisesAmount}`;
-        const inputName = `${this.#inputNames.sets}${this.#exercisesAmount}`;
-
-        exerciseWrapper.innerHTML = `
-            <button id="${upButtonId}" type="button">oben</button>
-            <button id="${downButtonId}" type="button">unten</button>
-            <button id="${deleteButtonId}" type="button">entfernen</button>
-            <label for="${selectName}">Übung</label>
-            <select id="${selectName}" name="${selectName}">
-                ${this.#globalExercises.length > 0 && `
-                    <optgroup label="Ausgewählte Übungen">
-                        ${this.#globalExercises.map((exercise) => `
-                            <option value="${exercise.ID}">${exercise.Name}</option>
-                        `)}
-                    </optgroup>    
-                `}
-                ${this.#userExercises.length > 0 && `
-                    <optgroup label="Deine Übungen">
-                        ${this.#userExercises.map((exercise) => `
-                            <option value="${exercise.ID}">${exercise.Name}</option>
-                        `)}
-                    </optgroup>
-                `}
-            </select>
-            <label for="${inputName}">Sets</label>
-            <input id="${inputName}" name="${inputName}" />
-        `;
+        const exercise = document.createElement('fit-edit-exercise-card');
+        exercise.setAttribute('exerciseId', String(this.#exercisesAmount));
 
         this.shadowRoot
             .getElementById(this.#ids.exercisesList)
-            .appendChild(exerciseWrapper);
-
-        const currentIndex = this.#exercisesAmount;
-        this.shadowRoot
-            .getElementById(deleteButtonId)
-            .addEventListener('click', () => this.deleteExercise(currentIndex));
+            .appendChild(exercise);
 
         this.#exercisesAmount += 1;
-    }
-
-    /** @param {number} index  */
-    deleteExercise(index) {
-        this.shadowRoot
-            .getElementById(`${this.#ids.exerciseWrapper}${index}`)
-            .remove();
     }
 }
 
---
+++ pages/workouts/components/EditExerciseCard.js
@@ -0,0 +1,154 @@
+import '/models/Workout.js';
+import '/models/Exercise.js';
+import '/models/ExerciseResponse.js';
+import { exercisesService } from '/services/ExercisesService.js';
+
+export class EditExerciseCard extends HTMLElement {
+    #ids = {
+        exerciseWrapper: 'exerciseWrapper',
+        upButton: 'upButton',
+        downButton: 'downButton',
+        deleteButton: 'deleteButton',
+    };
+
+    #inputNames = {
+        exercise: 'exercise',
+        sets: 'sets',
+    };
+
+    #exerciseId = 0;
+
+    /** @type {ExerciseResponse[]} */
+    #globalExercises = [];
+    /** @type {Exercise[]} */
+    #userExercises = [];
+
+    /** @type {string | null} */
+    #selectedExerciseId = null;
+    #setsAmount = '1';
+
+    constructor() {
+        super();
+
+        this.updateSelectedExercise = this.updateSelectedExercise.bind(this);
+        this.updateSetsAmount = this.updateSetsAmount.bind(this);
+        this.moveUp = this.moveUp.bind(this);
+        this.moveDown = this.moveDown.bind(this);
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.attachShadow({ mode: 'open' }).innerHTML = `
+            <style>
+                @import url('/globals.css');
+            </style>
+            <li></li>
+        `;
+    }
+
+    async connectedCallback() {
+        this.#exerciseId = Number(this.getAttribute('exerciseId'));
+
+        const wrapperId = `${this.#ids.exerciseWrapper}${this.#exerciseId}`;
+        const upButtonId = `${this.#ids.upButton}${this.#exerciseId}`;
+        const downButtonId = `${this.#ids.downButton}${this.#exerciseId}`;
+        const deleteButtonId = `${this.#ids.deleteButton}${this.#exerciseId}`;
+        const exerciseSelectName = `${this.#inputNames.exercise}${this.#exerciseId}`;
+        const setsInputName = `${this.#inputNames.sets}${this.#exerciseId}`;
+
+        this.#globalExercises = await exercisesService.getGlobalExercises();
+        this.#userExercises = await exercisesService.getUserExercises();
+
+        const wrapper = this.shadowRoot.querySelector('li');
+        wrapper.id = wrapperId;
+
+        wrapper.innerHTML = `
+            <button id="${upButtonId}" type="button">oben</button>
+            <button id="${downButtonId}" type="button">unten</button>
+            <button id="${deleteButtonId}" type="button">entfernen</button>
+            <label for="${exerciseSelectName}">Übung</label>
+            <select id="${exerciseSelectName}" name="${exerciseSelectName}">
+                ${this.#globalExercises.length > 0 && `
+                    <optgroup label="Ausgewählte Übungen">
+                        ${this.#globalExercises.map((exercise) => `
+                            <option value="${exercise.ID}">${exercise.Name}</option>
+                        `)}
+                    </optgroup>
+                `}
+                ${this.#userExercises.length > 0 && `
+                    <optgroup label="Deine Übungen">
+                        ${this.#userExercises.map((exercise) => `
+                            <option value="${exercise.ID}">${exercise.Name}</option>
+                        `)}
+                    </optgroup>
+                `}
+            </select>
+            <label for="${setsInputName}">Sets</label>
+            <input id="${setsInputName}" name="${setsInputName}" type="number" />
+        `;
+
+        const exerciseSelect = this.shadowRoot.getElementById(exerciseSelectName);
+
+        if (exerciseSelect instanceof HTMLSelectElement) {
+            exerciseSelect.value = this.#selectedExerciseId ?? this.#globalExercises[0].ID;
+            exerciseSelect.addEventListener('change', this.updateSelectedExercise);
+        }
+
+        const setsInput = this.shadowRoot.getElementById(setsInputName);
+
+        if (setsInput instanceof HTMLInputElement) {
+            setsInput.value = String(this.#setsAmount);
+            setsInput.addEventListener('change', this.updateSetsAmount);
+        }
+
+        this.shadowRoot
+            .getElementById(upButtonId)
+            .addEventListener('click', this.moveUp);
+
+        this.shadowRoot
+            .getElementById(downButtonId)
+            .addEventListener('click', this.moveDown);
+
+        this.shadowRoot
+            .getElementById(deleteButtonId)
+            .addEventListener('click', this.deleteExercise);
+    }
+
+    /** @param {Event} event */
+    updateSelectedExercise(event) {
+        if (event.currentTarget instanceof HTMLSelectElement) {
+            this.#selectedExerciseId = event.currentTarget.value;
+        }
+    }
+
+    /** @param {Event} event */
+    updateSetsAmount(event) {
+        if (event.currentTarget instanceof HTMLInputElement) {
+            this.#setsAmount = event.currentTarget.value;
+        }
+    }
+
+    moveUp() {
+        const previousExercise = this.previousElementSibling;
+
+        if (previousExercise === null) {
+            return;
+        }
+
+        previousExercise.before(this);
+    }
+
+    moveDown() {
+        const nextExercise = this.nextElementSibling;
+
+        if (nextExercise === null) {
+            return;
+        }
+
+        nextExercise.after(this);
+    }
+
+    deleteExercise() {
+        this.remove();
+    }
+}
+
+customElements.define('fit-edit-exercise-card', EditExerciseCard);
